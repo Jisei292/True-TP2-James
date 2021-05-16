1f1e6394a0a61f48c6d20fe90ef56f0fa0b63fbe
Jisei292 <73396433+Jisei292@users.noreply.github.com>
2021-05-16 12:07:38 -0400

    TP2 Finale

---
+++ README.md
@@ -0,0 +1,9 @@
+- James Sombounsing
+- Page Web 4w4
+- Une Page web en cours pour 4w4
+- admin
+Remettre sur LEA: Un fichier texte contenant 
+deux liens:
+-https://github.com/Jisei292/4w4_TP2_James (github) & http://localhost/4w4-3/
+(site webdev)
+-https://e1926527.webdev.cmaisonneuve.qc.ca/4w4/
---
+++ theme4w4_tp2_james/front-page.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme4w4
+ */
+
+get_header();
+?>
+	<main id="primary" class="site-main">
+	
+
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+			<section class="cours">
+			<!-- Carrousel -->
+			<?php
+			/* Start the Loop */
+            $precedent = "XXXXXX";
+			$chaine_bouton_radio = '';
+			//global $tProprieté;
+			while ( have_posts() ) :
+				the_post();
+                convertirTableau($tPropriété);
+				//print_r($tPropriété);
+				if ($tPropriété['typeCours'] != $precedent): 
+					if ("XXXXXX" != $precedent)	: ?>
+						</section>
+						<?php if (in_array($precedent, ['Web', 'Jeu'])): ?>
+							<section class="ctrl-carrousel">
+								<?php echo $chaine_bouton_radio;
+								$chaine_bouton_radio = '';
+								?>		
+							</section>
+						<?php endif; ?>
+					<?php endif; ?>	
+					<h2><?php echo $tPropriété['typeCours'] ?></h2>
+					<section <?php echo class_bloc($tPropriété['typeCours']) ?>>
+				<?php endif ?>	
+
+				<?php if (in_array($tPropriété['typeCours'],['Web', 'Jeu']) ) : 
+						get_template_part( 'template-parts/content', 'cours-carrousel' ); 
+						$chaine_bouton_radio .= '<input class="rad-carrousel"  type="radio" name="rad-'.$tPropriété['typeCours'].'">';
+					
+				elseif ($tPropriété['typeCours'] == 'Projets') : 
+
+						get_template_part( 'template-parts/content', 'galerie' );
+
+				else :		
+						get_template_part( 'template-parts/content', 'cours-article' ); 
+				endif;	
+				$precedent = $tPropriété['typeCours'];
+			endwhile;?>
+			</section> <!-- fin section cours -->
+
+<!-- ////////////////////////////////////////////////
+formulaire ajout d'article de categorie << Nouvelles >> -->
+		<section class="admin-rapid">
+			<h3>Ajouter un article de categorie "Nouvelles"</h3>
+			<input typ="text" name="title" placeholder="Titre">
+			<textarea name="content" placeholder="Contenu"></textarea>
+			<button id="bout-rapide">Créer une Nouvelle</button>
+		</section>
+			<section class="nouvelles"> <!-- les nouvelles -->
+			    <button id="bout_nouvelle"> Derniere Nouvelles</button>
+				<section>
+				</section>
+			</section>
+		<?php endif; ?>
+
+
+	
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
+
+function convertirTableau(&$tPropriété)
+{
+	/*
+	$titre = get_the_title(); 
+	// 582-1W1 Mise en page Web (75h)
+	$sigle = substr($titre, 0, 7);
+	$nbHeure = substr($titre,-4,3);
+	$titrePartiel =substr($titre,8,-6);
+	$session = substr($titre, 4,1);
+	// $contenu = get_the_content();
+	// $resume = substr($contenu, 0, 200);
+	$typeCours = get_field('type_de_cours');
+*/
+
+	$tPropriété['titre'] = get_the_title(); 
+	$tPropriété['sigle'] = substr($tPropriété['titre'], 0, 7);
+	$tPropriété['nbHeure'] = substr($tPropriété['titre'],-4,3);
+	$tPropriété['titrePartiel'] = substr($tPropriété['titre'],8,-6);
+	$tPropriété['session'] = substr($tPropriété['titre'], 4,1);
+	$tPropriété['typeCours'] = get_field('type_de_cours');
+}
+
+function genere_bouton_radio($type)
+{
+	
+}
+
+
+function class_bloc($type_de_cours){
+	if (in_array($type_de_cours,['Web', 'Jeu'])) {
+		return ('class = "carrousel-2"');
+
+	}
+	elseif($type_de_cours == 'Projets'){
+		return('class = "galerie"');
+	}
+
+	else {
+		return('class = "bloc"');
+	}
+
+
+}
